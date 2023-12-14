a9dd95368b421a64d55da533328eb2eb7e99ea51
IraGrafova <i.grafova@ntc-hitech.ru>
2023-12-14 11:27:48 +0300

    do products

---
+++ src/data/products.js
@@ -0,0 +1,62 @@
+export default [
+  {
+    title: "Радионяня Motorola MBP16",
+    price: 3690,
+    image: "./img/radio.jpg",
+  },
+  {
+    title: "Ультразвуковая зубная щётка Playbrush Smart Sonic",
+    price: 5960,
+    image: "./img/toothbrush.jpg",
+  },
+  {
+    title: "Смартфон Xiaomi Mi Mix 3 6/128GB",
+    price: 21000,
+    image: "./img/phone.jpg",
+  },
+  {
+    title: "Электроскейт Razor Cruiser",
+    price: 23690,
+    image: "./img/board.jpg",
+  },
+  {
+    title: "Смартфон Xiaomi Mi A3 4/64GB Android One",
+    price: 14500,
+    image: "img/phone-2.jpg",
+  },
+  {
+    title: "Смартфон Xiaomi Redmi 6/128GB",
+    price: 8960,
+    image: "img/phone-3.jpg",
+  },
+  {
+    title: "Электрический дрифт-карт Razor Crazy Cart",
+    price: 39690,
+    image: "./img/bicycle.jpg",
+  },
+  {
+    title: "Детский трюковой самокат Razor Grom",
+    price: 4790,
+    image: "./img/scooter.jpg",
+  },
+  {
+    title: "Роллерсёрф Razor RipStik Air Pro",
+    price: 6690,
+    image: "./img/ripstik.jpg",
+  },
+  {
+    title: "Наушники AirPods с беспроводным зарядным футляром",
+    price: 16500,
+    image: "./img/airpods.jpg",
+  },
+  {
+    title: "Наушники Sony",
+    price: 34690,
+    image: "./img/headphones.jpg",
+  },
+  {
+    title: "Сушильная машина с инвертором и ультрафиолетом Weissgauff",
+    price: 64000,
+    image: "./img/sushilnaya_mashina.jpeg",
+  },
+];
