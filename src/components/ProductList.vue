756fd6e5edf6cf85afdacc612c2b22bad37e71c9
IraGrafova <i.grafova@ntc-hitech.ru>
2023-12-14 17:30:36 +0300

    do ProductItem

---
+++ src/components/ProductList.vue
@@ -0,0 +1,22 @@
+<template>
+  <ul class="catalog__list">
+    <ProductItem
+      :product="item"
+      v-for="(item, index) in products"
+      :key="index"
+    />
+  </ul>
+</template>
+
+<script>
+import ProductItem from "./ProductItem.vue";
+
+export default {
+  name: "ProductList",
+
+  components: { ProductItem },
+  props: { products: Array },
+
+
+};
+</script>
---
+++ src/constans.js
@@ -0,0 +1,4 @@
+const message = 'Hello';
+const messageTwo = 'Goodbye';
+
+export {message, messageTwo};
