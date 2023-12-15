0eac667a87c434e2736616ba8660f32ee423e05f
IraGrafova <i.grafova@ntc-hitech.ru>
2023-12-15 12:43:40 +0300

    do alert

--- src/main.js
+++ src/main.js
@@ -1,10 +1,21 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
 
-import data from './data'
+import data from "./data";
+import { message, messageTwo } from "./constans";
+import alertMessage from "./utils";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: (h) => h(App),
+  methods: {
+    alertMessages() {
+      alertMessage(message);
+      alertMessage(messageTwo);
+    },
+  },
+  mounted() {
+    this.alertMessages()
+  },
+}).$mount("#app");
