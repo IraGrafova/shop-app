2b6cfa7d150f58defda6bedb29a98074d67967a6
IraGrafova <i.grafova@ntc-hitech.ru>
2023-12-20 11:21:27 +0300

    do resetForm

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,7 @@
     </div>
 
     <div class="content__catalog">
-      <ProductFilter />
+      <ProductFilter :price-from.sync="filterPriceFrom" :price-to.sync="filterPriceTo" :category-id.sync="filterCategoryId"/>
       <section class="catalog">
         <ProductList :products="products" />
         <BasePagination
