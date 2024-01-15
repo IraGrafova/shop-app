fa5a1a5fdcb10289162d064b1e05c780b6daff75
IraGrafova <i.grafova@ntc-hitech.ru>
2024-01-15 17:19:17 +0300

    do routes

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,7 @@
 <template>
-  <component :is="currentPageComponent" :page-params="currentPageParams"/>
+  <router-view />
 </template>
 
 <script>
-import MainPage from "./pages/MainPage";
-import ProductPage from "./pages/ProductPage";
-import NotFoundPage from "./pages/NotFoundPage";
-import eventBus from "./eventBus";
-
-const routes = {
-  main: "MainPage",
-  product: "ProductPage",
-};
-
-export default {
-  data() {
-    return {
-      currentPage: "main",
-      currentPageParams: {}
-    };
-  },
-  methods: {
-    gotoPage(pageName, pageParams) {
-      this.currentPage = pageName;
-      this.currentPageParams = pageParams || {};
-    }
-  },
-  computed: {
-    currentPageComponent() {
-      return routes[this.currentPage] || 'NotFoundPage';
-    },
-  },
-  components: { MainPage, ProductPage, NotFoundPage },
-  created() {
-    eventBus.$on('gotoPage', (pageName, pageParams) => this.gotoPage(pageName, pageParams))
-  }
-};
+export default {};
 </script>
--- src/pages/ProductPage.vue
+++ src/pages/ProductPage.vue
@@ -3,14 +3,14 @@
     <div class="content__top">
       <ul class="breadcrumbs">
         <li class="breadcrumbs__item">
-          <a class="breadcrumbs__link" href="#" @click.prevent="gotoPage('main')">
+          <router-link class="breadcrumbs__link" :to="{name: 'main'}">
             Каталог
-          </a>
+          </router-link>
         </li>
         <li class="breadcrumbs__item">
-          <a class="breadcrumbs__link" href="#">
+          <router-link class="breadcrumbs__link" :to="{name: 'main'}">
            {{ category.title }}
-          </a>
+          </router-link>
         </li>
         <li class="breadcrumbs__item">
           <a class="breadcrumbs__link">
@@ -179,22 +179,23 @@ import products from '@/data/products';
 import categories from '@/data/categories';
 import gotoPage from '@/helpers/gotoPage';
 import numberFormat from '@/helpers/numberFormat';
+import router from '@/router';
 
 export default {
-    props: ['pageParams'],
     filters: {
-      numberFormat
+        numberFormat
     },
     computed: {
-      product() {
-        return products.find(product => product.id === this.pageParams.id)
-      },
-      category() {
-        return categories.find(category => category.id === this.product.categoryId)
-      }
+        product() {
+            return products.find(product => product.id == +this.$route.params.id); // с тремя равно не работает ( Number тоже не помогло )
+        },
+        category() {
+            return categories.find(category => category.id === this.product.categoryId);
+        }
     },
     methods: {
-      gotoPage
-    }
+        gotoPage
+    },
+    components: { router }
 }
 </script>
