e0186eeb04581939e5b7320579f8fca78a36b71d
IraGrafova <i.grafova@ntc-hitech.ru>
2024-01-15 23:55:32 +0300

    cartindicator

---
+++ src/components/CartIndicator.vue
@@ -0,0 +1,16 @@
+<template>
+    <router-link class="header__cart" href="cart.html" aria-label="Корзина с товарами" :to="{name: 'cart'}">
+        <svg width="30" height="21" fill="currentColor">
+          <use xlink:href="#icon-cart"></use>
+        </svg>
+        <span class="header__count" aria-label="Количество товаров">3</span>
+      </router-link>  
+</template>
+
+<script>
+import router from '@/router';
+
+    export default {
+    components: { router }
+}
+</script>
